104e122b0a07671404b5de3378a91c5d9a87c94a
Delio Brignoli <dbrignoli@audioscience.com>
2019-07-15 11:13:30 +0100

    Fix subsys deallocation with nested withSubSys

--- src/hpi/fancy.nim
+++ src/hpi/fancy.nim
@@ -27,42 +27,47 @@ proc raiseHPIError(hpi_error_num: raw.hpi_err_t) {.raises: [HPIError].} =
 # Subsys life cycle
 
 var hpi_subsys_global: hpi_subsys_handle = nil
-proc getHpiSubSys(): hpi_subsys_handle =
+proc getHpiSubSys(): (hpi_subsys_handle, bool) =
     if hpi_subsys_global == nil:
         hpi_subsys_global = raw.HPI_SubSysCreate()
-    result = hpi_subsys_global
+        result = (hpi_subsys_global, true)
+    else:
+        result = (hpi_subsys_global, false)
 
 proc free*(hs: hpi_subsys_handle) =
-    raw.HPI_SubSysFree(hs)
+    if hs != nil:
+        raw.HPI_SubSysFree(hs)
+        hpi_subsys_global = nil
 
 template withSubSys*(body: untyped) =
-    let hs = getHpiSubSys()
+    let (hs, created) = getHpiSubSys()
     try:
         body
     finally:
-        free(hs)
+        if created:
+            free(hs)
 
 # Adapter life cycle
 
 proc adapter_open*(adapter_index: uint): Adapter =
-    let hs = getHpiSubSys()
+    let (hs, _) = getHpiSubSys()
     let index: uint16 = uint16(adapter_index)
     raiseHPIError raw.HPI_AdapterOpen(hs, index)
     result = Adapter(index: index)
 
 proc close*(adapter: Adapter) =
-    let hs = getHpiSubSys()
+    let (hs, _) = getHpiSubSys()
     raiseHPIError raw.HPI_AdapterClose(hs, adapter.index)
 
 # Adapter functions
 
 proc get_mode*(adapter: Adapter): adapter_modes =
     var current_adapter_mode: uint32 = 0
-    let hs = getHpiSubSys()
+    let (hs, _) = getHpiSubSys()
     raiseHPIError raw.HPI_AdapterGetMode(hs, adapter.index, addr current_adapter_mode)
     result = adapter_modes(current_adapter_mode)
 
 proc set_mode_ex*(adapter: Adapter, adapter_mode: adapter_modes, call_mode: adapter_mode_cmds) =
     var current_adapter_mode: uint32 = 0
-    let hs = getHpiSubSys()
+    let (hs, _) = getHpiSubSys()
     raiseHPIError raw.HPI_AdapterSetModeEx(hs, adapter.index, uint32(adapter_mode), uint16(call_mode))
